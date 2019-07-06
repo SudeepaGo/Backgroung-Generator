7b4ddc86de4ab28e5757096a647921ebacb4c5c9
Sudeepa Gorle <sudeepago1994@gmail.com>
2019-07-06 22:25:32 +0530

    added option to download background

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const css = document.querySelector("h3");
 const leftColor = document.getElementById("leftGradient");
 const rightColor = document.getElementById("rightGradient");
 const body = document.body;
+const canvas = document.querySelector("canvas");
 
 const setBackground = () => {
   body.style.background = `linear-gradient(to right, ${leftColor.value}, ${
@@ -11,7 +12,9 @@ const setBackground = () => {
 };
 
 const setText = () => {
-  css.textContent = body.style.background;
+  if (body.style.background) css.textContent = body.style.background;
+  else
+    css.textContent = `linear-gradient(to right, rgb(45, 209, 192), rgb(181, 38, 217))`;
 };
 
 setText();
@@ -20,11 +23,22 @@ leftColor.addEventListener("input", setBackground);
 
 rightColor.addEventListener("input", setBackground);
 
-var a = 1;
-
-function change() {
-  a = 2;
-  console.log(a);
-}
-change();
-console.log(a);
+downloadImage = () => {
+  const ctx = canvas.getContext("2d");
+  const w = canvas.width;
+  const h = canvas.height;
+  const bkgGradient = ctx.createLinearGradient(0, 0, w, h);
+  bkgGradient.addColorStop(0, leftColor.value);
+  bkgGradient.addColorStop(1, rightColor.value);
+  ctx.fillStyle = bkgGradient;
+  ctx.fillRect(0, 0, w, h);
+  let url = canvas.toDataURL();
+  // url = url.replace(/^data:image\/[^;]+/, "data:application/octet-stream");
+  // window.open(url);
+  // location.href = url;
+  // Does not work in IE/Safari/Opera
+  const link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.setAttribute("download", "background.png");
+  link.click();
+};
